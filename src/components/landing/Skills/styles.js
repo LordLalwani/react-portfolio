67aca8734da314bd88f1a7f88938218a70fe7955
Deep Lalwani <deepplalwani@hotmail.com>
2019-08-20 23:39:22 +1200

    Updated Content & added skill cards

--- src/components/landing/Skills/styles.js
+++ src/components/landing/Skills/styles.js
@@ -41,6 +41,59 @@ export const Details = styled.div`
 		color: #707070;
 	}
 `
+export const Content = styled.div`
+	padding: 0rem 0rem;
+	h5 {
+		text-align: center;
+		color: #Black;
+		display: block;
+		font-size: 0.83em;
+		margin-block-start: 0.5em;
+		margin-block-end: 0.5em;
+		margin-inline-start: 0%;
+		margin-inline-end: 0%;
+		font-weight: bold;
+	}
+`
+
+export const Item = styled.div`
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.11);
+	border-radius: 5px;
+	  
+	:hover {
+		color: white;
+		div {
+			background-color: #6c62ff;
+		}
+	  }
+
+	h4 {
+		color: #212121;
+	}
+
+	p {
+		color: #707070;
+	}
+`
+
+export const Grid = styled.div`
+	display: grid;
+	align-items: center;
+	grid-template-columns: repeat(3, 1fr);
+	grid-template-rows: 10fr;
+	gap: 0.8rem 0.8rem;
+
+	@media (max-width: 960px) {
+		grid-template-columns: repeat(2, 1fr);
+	}
+
+	@media (max-width: 680px) {
+		grid-template-columns: 1fr;
+	}
+`
 
 export const Thumbnail = styled.div`
 	flex: 1;
--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Wrapper } from './styles'
-import { ButtonStyleTwo } from 'Common'
 
 const NavbarLinks = ({ desktop }) => (
 	<Wrapper desktop={desktop}>
-		{/* <ButtonStyleTwo as={AnchorLink}> Download CV </ButtonStyleTwo> */}
-		<AnchorLink href="#about">About</AnchorLink>
+		<AnchorLink href="#about">Skills</AnchorLink>
 		<AnchorLink href="#projects">Projects</AnchorLink>
 		<AnchorLink href="#contact">Contact</AnchorLink>
 	</Wrapper>
