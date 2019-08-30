d6ac8a14ccce8a059378cea8a747d5a263f6346c
LordLalwani <52875534+LordLalwani@users.noreply.github.com>
2019-08-31 08:14:20 +1200

    Implemented downloadable CV

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,61 +1,92 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Button = styled.button`
-	cursor: pointer;
-	border-radius: 3px;
-	padding: 0.7rem 2.5rem;
-	border: none;
-	-webkit-appearance: none;
-	-webkit-touch-callout: none;
-	-webkit-user-select: none;
-	-khtml-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-	color: #fff;
-	background: #0074d9;
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 0.7rem 2.5rem;
+  border: none;
+  -webkit-appearance: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background: #0074d9;
 
-	&:focus {
-		outline: none;
-	}
+  &:focus {
+    outline: none;
+  }
 
-	&:disabled {
-		background: gray;
-	}
+  &:disabled {
+    background: gray;
+  }
 
-	${({ secondary }) =>
-		secondary &&
-		`
+  ${({ secondary }) =>
+    secondary &&
+    `
 		background: #001F3F;
 	`}
-`
+`;
 
 export const ButtonStyleTwo = styled.button`
-	cursor: pointer;
-	border-radius: 3px;
-	padding: 0.6rem 2.5rem;
-	margin: 0px 0px;
-	border: none;
-	-webkit-appearance: none;
-	-webkit-touch-callout: none;
-	-webkit-user-select: none;
-	-khtml-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-	color: #fff;
-	background: #6c62ff;
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 0.6rem 2.5rem;
+  margin: 0px 0px;
+  border: none;
+  -webkit-appearance: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background: #6c62ff;
 
-	&:focus {
-		outline: none;
-	}
+  &:focus {
+    outline: none;
+  }
 
-	&:disabled {
-		background: gray;
-	}
+  &:disabled {
+    background: gray;
+  }
 
-	&:hover {
-		background: black;
-	}
-`
+  &:hover {
+    background: black;
+  }
+`;
 
+export const ButtonStyleThree = styled.button`
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 0.6rem 2.5rem;
+  margin: 0px 0px;
+  border: none;
+  -webkit-appearance: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background: #6c62ff;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background: gray;
+  }
+
+  &:hover {
+    background: black;
+  }
+  a {
+    color: white !important;
+  }
+`;
