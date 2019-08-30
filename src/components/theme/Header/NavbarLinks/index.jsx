5751679b280bf604946c2f9141535eacf40f3fd7
Deep Lalwani <deepplalwani@hotmail.com>
2019-08-31 08:10:16 +1200

    Implmented downloadable cv

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Wrapper } from './styles'
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { Wrapper } from "./styles";
+import { ButtonStyleThree } from "../../../common/Button";
 
 const NavbarLinks = ({ desktop }) => (
-	<Wrapper desktop={desktop}>
-		<AnchorLink href="#about">Skills</AnchorLink>
-		<AnchorLink href="#projects">Projects</AnchorLink>
-		<AnchorLink href="#contact">Contact</AnchorLink>
-	</Wrapper>
-)
+  <Wrapper desktop={desktop}>
+    <AnchorLink href="#about">Skills</AnchorLink>
+    <AnchorLink href="#projects">Projects</AnchorLink>
+    <AnchorLink href="#contact">Contact</AnchorLink>
+    <ButtonStyleThree>
+      <a href="#" onClick={e => downloadCV(e)}>
+        Download CV
+      </a>
+    </ButtonStyleThree>
+  </Wrapper>
+);
 
-export default NavbarLinks
+const downloadCV = () => {
+  window.open(require("../../../../../static/DeepLalwaniCV.pdf"), "_none");
+};
+export default NavbarLinks;
